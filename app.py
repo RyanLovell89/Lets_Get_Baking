7cbee5e0b46c54bc37dd28e29e8fefc83da74cec
Ryan Lovell <rjl89@live.co.uk>
2020-11-24 15:57:41 +0000

    created sign up functionality

--- app.py
+++ app.py
@@ -28,8 +28,25 @@ def index():
 
 @app.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
-    return render_template("sign_up.html")
+    if request.method == "POST":
+        # checks to see if username already exists in mongo database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
 
+        if existing_user:
+            flash("Username Already Taken")
+            return redirect(url_for("sign_up"))
+
+        sign_up = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(sign_up)
+
+        # places the user into a session cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Sign Up Successful")
+    return render_template("sign_up.html")
 
 
 if __name__ == "__main__":
