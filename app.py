282af4ae631e272b8cf5cfd900eb2e1194dce297
Ryan Lovell <rjl89@live.co.uk>
2020-11-25 15:04:20 +0000

    created logout function and sorted nav bar links to show correctly when logged in or logged out

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def login():
         if existing_user:
             # checks to see if the username and password match
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
+                    existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
                 flash("Welcome, {}".format(
                     request.form.get("username")))
@@ -80,11 +80,20 @@ def login():
     return render_template("login.html")
 
 
+# profile function
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
     return render_template("profile.html", username=username)
+
+
+# logout function
+@app.route("/logout")
+def logout():
+    flash("You have logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
